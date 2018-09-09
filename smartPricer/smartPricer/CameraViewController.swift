7c9d555bdb60fd3b664458c28a395b9557a22c37
Justin Antony <justinantony92@gmail.com>
2018-09-09 11:12:45 -0400

    Started work on app in earnest. Defined the three main views that will define the iOS app. A Home view (list already priced items), and two views to upload images to the ML model (a camera view and a view to upload already taken images from the camera roll). Next step: implement code for the camera and camera roll views to allow them to add images to the home view.

--- smartPricer/smartPricer/CameraViewController.swift
+++ smartPricer/smartPricer/CameraViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SecondViewController: UIViewController {
+class CameraViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet var previewView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
