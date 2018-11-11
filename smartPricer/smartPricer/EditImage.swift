b63d7789c84a0c627b9cda067c2c7039c46ba48b
Justin Antony <justinantony92@gmail.com>
2018-11-11 17:33:41 -0500

    Started implementing view to edit images and add them to list on "My Stuff" view.

---
+++ smartPricer/smartPricer/EditImage.swift
@@ -0,0 +1,34 @@
+//
+//  EditImage.swift
+//  smartPricer
+//
+//  Created by Justin Antony on 2018-11-11.
+//  Copyright © 2018 Justin Antony. All rights reserved.
+//
+
+import UIKit
+
+class EditImage: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let ImagePicker = UIImagePickerController()
+
+    @IBOutlet var ViewImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
